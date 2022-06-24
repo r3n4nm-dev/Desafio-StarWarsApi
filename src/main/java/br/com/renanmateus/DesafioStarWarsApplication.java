8cb1e440fb66bf5a171b692fd0ec386c04c313e3
r3n4nm-dev <r3n4nm@gmail.com>
2022-06-24 09:18:16 -0300

    initial structure

---
+++ src/main/java/br/com/renanmateus/DesafioStarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.renanmateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioStarWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/renanmateus/DesafioStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renanmateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
